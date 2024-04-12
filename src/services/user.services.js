d7d9f8febcaeb5d9afe7174ac14008902eaafe6e
Nadine Rangosch <nrangosch@gmail.com>
2024-04-12 13:19:14 +0200

    created user services

---
+++ src/services/user.services.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+ 
+class UserService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5005'
+    });
+ 
+    // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use((config) => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem("authToken");
+ 
+      if (storedToken) {
+        console.log("setting headers...", storedToken)
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+ 
+      return config;
+    });
+  }
+ 
+
+ 
+  // GET all users
+  getAllUsers = () => {
+    return this.api.get('/api/user');
+  };
+ 
+  // GET one user
+  getUser = id => {
+    return this.api.get(`/api/user/${id}`);
+  };
+ 
+  // UPDATE a user
+  updateUser = (id, requestBody) => {
+    return this.api.put(`/api/user/${id}`, requestBody);
+  };
+ 
+  // DELETE a user
+  deleteUser = id => {
+    return this.api.delete(`/api/user/${id}`);
+  };
+}
+
+
+const userService = new UserService()
+
+export default userService
