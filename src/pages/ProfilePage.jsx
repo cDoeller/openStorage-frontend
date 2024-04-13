0daff0e7b883f4f7874277b07271128a4beb0852
Nadine Rangosch <nrangosch@gmail.com>
2024-04-13 10:24:10 +0200

    added artist profile features

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -52,6 +52,40 @@ function ProfilePage() {
             </div>
           </div>
 
+          {userInfo.isArtist && (
+            <div>
+            <div className="artist-statement-wrapper">
+              <h5>Artist Statement:</h5>
+              <p>{userInfo.artist_statement}</p>
+            </div>
+            <div>
+            <h5>Artworks</h5>
+            {/* {userInfo.artworks && 
+            userInfo.artworks.map((oneArtwork, index)=>{
+              return (
+                <div key={index} className="artwork-card-wrapper">
+      <div className="artwork-card-image-wrapper">
+        <img src={oneArtwork.images_url[0]} alt={oneArtwork.title} />
+      </div>
+      <div className="artwork-card-info-wrapper">
+        <div className="artwork-card-info-text-wrapper">
+          <p className="artwork-card-info-text-text">
+          {oneArtwork.title}
+          </p>
+
+        </div>
+        <div className="artwork-card-icon-wrapper">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+              )
+            })} */}
+            </div>
+
+            </div>
+          )}
+
           
           <div className="loans-wrapper">
             <h5>Current Loans</h5>
