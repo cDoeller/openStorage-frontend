f724eceb5067c9fb936eeeaa0d199aa1ffb595a5
Christian Doeller <mail@christiandoeller.de>
2024-04-25 11:34:47 +0200

    some dummy data for faq page

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -3,21 +3,22 @@ import "../styles/FAQPage.css";
 import FAQQuestion from "../components/FAQQuestion";
 
 function FAQPage() {
+  const question = "How can I get veryfied as an artist?";
+  const answer =
+    "You can click on the *more* button in your profile menu bar and make a request for an artist profile. On the following form, you will have to enter more information about you as an artist. This information will be validated by our team. We will notify you as soon as possible about the outcome of your request!";
   return (
     <div className="page-wrapper mobile-dvh">
       <div className="faq-questions-wrapper">
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
-        <FAQQuestion question={"question"} answer={"answer"}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
+        <FAQQuestion question={question} answer={answer}></FAQQuestion>
       </div>
     </div>
   );
