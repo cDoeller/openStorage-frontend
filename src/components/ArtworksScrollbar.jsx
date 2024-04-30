444012964841c48f540cfc80ed9c5155846aebca
Nadine Rangosch <nrangosch@gmail.com>
2024-04-30 20:31:38 +0200

    refactored artworks scrollbar, found problem with backend population

--- src/components/ArtworksScrollbar.jsx
+++ src/components/ArtworksScrollbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import "../styles/ArtworksScrollbar.css";
 import ArtworkScrollbarCard from "./ArtworkScrollbarCard";
 import { AuthContext } from "../context/auth.context";
@@ -8,6 +8,33 @@ function ArtworksScrollbar(props) {
   const { user, isLoggedIn } = useContext(AuthContext);
 
   const { userInfo, heading, contents } = props;
+
+  const [artworksArray, setArtworksArray] = useState([])
+
+  useEffect(()=>{
+    let filteredContents = contents.filter((element)=>{
+      if(heading==="Artworks"){
+        console.log(element)
+        return element
+      }
+      if(heading==="Current Rentals" || heading === "Pending Requests"){
+        console.log("rentals", element)
+        if(element.status === "accepted"){
+          console.log("rented: ", element)
+          return element.artwork
+        }
+        if(element.status === "pending"){
+          console.log("pending: ", element)
+          return element.artwork
+        }
+      }
+
+    })
+    console.log(heading, filteredContents)
+    setArtworksArray(filteredContents)
+
+
+  }, [heading, contents])
 
   return (
     <div className="artworks-scrollbar-container">
@@ -22,15 +49,15 @@ function ArtworksScrollbar(props) {
         )}
       </div>
       <div className="profile-artworks-scrollbar">
-        {contents &&
-          contents.map((oneRental, index) => {
+        {artworksArray &&
+          artworksArray.map((oneArtwork, index) => {
             return (
               <div key={index} className="profile-artwork-card-wrapper">
                 <ArtworkScrollbarCard
                 heading = {heading}
-                  title={oneRental.title}
-                  id={oneRental._id}
-                  img={oneRental.images_url[0]}
+                  title={oneArtwork.title}
+                  id={oneArtwork._id}
+                  img={oneArtwork.images_url[0]}
                 />
               </div>
             );
