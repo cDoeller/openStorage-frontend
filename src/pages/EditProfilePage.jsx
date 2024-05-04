e2fe66b0eb04e62380c7ec65f12a75e2cd0a67cd
Nadine Rangosch <nrangosch@gmail.com>
2024-05-04 18:42:29 +0200

    fixed image uploading for create and edit artwork, fixed edit profile page

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import Select from "react-select";
 import { AuthContext } from "../context/auth.context";
 import "../styles/EditProfile.css";
 import userService from "../services/user.services";
 import uploadService from "../services/file-upload.services";
 import "../styles/Forms.css"
+import germanCities from "../data/cities-germany.json"
 
 function EditProfilePage() {
   const navigate = useNavigate();
@@ -12,6 +14,7 @@ function EditProfilePage() {
   const storedToken = localStorage.getItem("authToken");
 
   const [userInfo, setUserInfo] = useState(null);
+  const [cityOptions, setCityOptions] = useState([])
 
   const [realName, setRealName] = useState("");
   const [profileImg, setProfileImg] = useState("");
@@ -31,9 +34,51 @@ function EditProfilePage() {
   const [addressPostcode, setAddressPostcode] = useState(null);
   const [phoneNumber, setPhoneNumber] = useState(null);
 
+  function handleCitiesSelectChange(selectedOption) {
+    setCity(selectedOption.value);
+  }
+
+  // REACT SELECT STYLING
+  // https://react-select.com/styles#inner-components
+  const selectStles = {
+    control: (baseStyles, state) => ({
+      ...baseStyles,
+      border: "none",
+      outline: "red",
+      borderRadius: "0",
+    }),
+    container: (baseStyles, state) => ({
+      ...baseStyles,
+      outline: "red",
+      border: "none",
+      borderRadius: "0",
+      borderBottom: "2px solid black",
+    }),
+    dropdownIndicator: (baseStyles, state) => ({
+      ...baseStyles,
+      border: "none",
+      outline: "none",
+      color: "black",
+    }),
+    menu: (baseStyles, state) => ({
+      ...baseStyles,
+      margin: "0",
+      borderRadius: "0",
+      maxHeight: "7rem",
+      overflow: "scroll",
+    }),
+  };
+
+  
+
   function handleProfileImg(e){
     let imageToUpload = new FormData()
     imageToUpload.append("images", e.target.files[0])
+
+    
+    const preview = URL.createObjectURL(e.target.files[0]);
+    console.log(preview);
+    setProfileImg(preview)
 
     setImageData(imageToUpload)
   }
@@ -76,6 +121,14 @@ function EditProfilePage() {
 
   }
 
+  useEffect(()=>{
+    let formattedOptions = germanCities.map((oneCity)=>{
+      return {value:oneCity.city, label:oneCity.city}
+    })
+    setCityOptions(formattedOptions)
+
+  },[])
+  
   useEffect(() => {
     if (user) {
       userService
@@ -103,7 +156,7 @@ function EditProfilePage() {
   }, [user, storedToken]);
 
   return (
-    <div id="ProfilePage" className="page-wrapper mobile-dvh">
+    <div id="EditProfilePage" className="page-wrapper mobile-dvh">
     <div className="edit-profile heading-wrapper">
       <h1>Edit Profile</h1>
       <button className="back-button" onClick={(e)=>{e.preventDefault(); navigate(-1)}}> {"< Back"}</button>
@@ -112,10 +165,14 @@ function EditProfilePage() {
       {isLoggedIn && userInfo && (
         <form className="profile edit-form" onSubmit={(e)=>{handleSubmit(e)}}>
           <div className="edit-profile-info-wrapper">
+          <div className="change-profile-img-container">
             <div className="edit-profile-img-wrapper">
               <img src={profileImg} alt="profile image" />
-              <input type="file" onChange={(e)=>{handleProfileImg(e)}} />
             </div>
+
+              <input name="profile-img" className="file-input" type="file" accept=".jpg, .png" onChange={(e)=>{handleProfileImg(e)}} />
+
+          </div>
             <div className="edit-profile-text-wrapper">
               {userInfo.isArtist ? (
                 <p className="profile-type">Artist</p>
@@ -141,16 +198,16 @@ function EditProfilePage() {
                   setTagline(e.target.value);
                 }}
               />
-              <select
-                className="edit-profile-input"
-                id="city"
-                value={city}
-                onChange={(e) => {
-                  setCity(e.target.value);
-                }}
-              >
-                <option value="Leipzig">Leipzig</option>
-              </select>
+              {/* CITY */}
+            <label htmlFor="" className="filterinterface-form-label">
+              City
+            </label>
+            <Select
+              options={cityOptions}
+              onChange={handleCitiesSelectChange}
+              value={{ label: city }}
+              styles={selectStles}
+            />
             </div>
           </div>
 
