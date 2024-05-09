ae2ffdc87f878d63cd08095faa79bceb871fd2bd
Christian Doeller <mail@christiandoeller.de>
2024-05-09 15:13:10 +0200

    no favs selected

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import "../styles/styles-pages/FavoritesPage.css";
 import { AuthContext } from "../context/auth.context";
 import userService from "../services/user.services";
@@ -20,11 +20,25 @@ function FavoritesPage() {
       .catch((err) => console.log(err));
   }, [user]);
 
+  const noFavoritesElement = (
+    <>
+      <div className="favorites-none-wrapper">
+        <div className="favorites-none-inner">
+          <div className="favorites-none-icon-wrapper">
+            <img src="/img/star-icon.png" alt="" />
+          </div>
+          <h3 className="favorites-none-text">No Favorites Selected</h3>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <div className="page-wrapper mobile-dvh">
-      <div className="favorites-wrapper">
-        {favorites &&
-          favorites.map((artwork) => {
+      {favorites && favorites.length === 0 && noFavoritesElement}
+      {favorites && favorites.length > 0 && (
+        <div className="favorites-wrapper">
+          {favorites.map((artwork) => {
             return (
               <Link to={`/artworks/${artwork._id}`} key={artwork._id}>
                 <ArtworkCardFavorites
@@ -37,7 +51,8 @@ function FavoritesPage() {
               </Link>
             );
           })}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
