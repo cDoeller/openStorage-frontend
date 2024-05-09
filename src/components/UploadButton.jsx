162903c50f103bff9ca0c92aefa396142a90af0d
Nadine Rangosch <nrangosch@gmail.com>
2024-05-09 14:12:58 +0200

    new upload button on create artwork page

---
+++ src/components/UploadButton.jsx
@@ -0,0 +1,47 @@
+import "../styles/styles-components/UploadButton.css"
+import { useRef } from "react";
+
+function FileUploader({handleFileUpload}){
+
+    const hiddenFileInput = useRef(null);
+
+    const handleClick = event => {
+        event.preventDefault()
+        hiddenFileInput.current.click(); 
+    };
+
+    const handleChange = event => {
+        event.preventDefault()
+        const files = event.target.files;
+        handleFileUpload(files);                   // ADDED
+      };
+
+
+    return (
+      <>
+        <button
+        className="button-upload"
+        onClick={(event)=>handleClick(event)} 
+        >
+          Upload a file
+        </button>
+
+        <input
+          ref={hiddenFileInput}
+            name="images"
+            className="file-input"
+            type="file"
+            accept=".jpg, .png"
+            multiple
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            style={{display:'none'}}
+          />
+  
+
+      </>
+    );
+  }
+
+  export default FileUploader
